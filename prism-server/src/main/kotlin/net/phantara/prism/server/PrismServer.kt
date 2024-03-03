c7eb8b2c7d43c2de4fd2809c6ae7537ebfdda8dc
Lisa Kapahnke <cythantv@gmail.com>
2024-03-03 17:14:25 +0100

    Finished publishing of api and PrismServer

--- prism-server/src/main/kotlin/net/phantara/prism/server/PrismServer.kt
+++ prism-server/src/main/kotlin/net/phantara/prism/server/PrismServer.kt
@@ -1,5 +1,10 @@
 package net.phantara.prism.server
 
+import net.minestom.server.MinecraftServer
+import net.minestom.server.extensions.ExtensionManager
+import net.minestom.server.extras.MojangAuth
+import net.minestom.server.extras.bungee.BungeeCordProxy
+import net.minestom.server.extras.velocity.VelocityProxy
 import net.phantara.prism.api.PrismServerAPI
 import net.phantara.prism.server.api.impl.InstanceFactoryImpl
 import net.phantara.prism.server.api.impl.SchematicFactoryImpl
@@ -28,10 +33,56 @@ class PrismServer {
     init {
         instance = this
         this.properties = PrismServerProperties()
+
+        val server = MinecraftServer.init()
+
+        val extensionManager = ExtensionManager(MinecraftServer.process())
+        extensionManager.start()
+
+        extensionManager.gotoPreInit()
+
         PrismServerAPI(
             InstanceFactoryImpl(),
             ServerPropertiesImpl(),
             SchematicFactoryImpl()
         )
+
+        setupProperties()
+
+        extensionManager.gotoInit()
+
+        System.setProperty("minestom.chunk-view-distance", "10")
+        System.setProperty("minestom.entity-view-distance", "32")
+
+        MinecraftServer.setBrandName("PrismServer")
+        MinecraftServer.getSchedulerManager().buildShutdownTask {
+            extensionManager.shutdown()
+        }
+
+        server.start(PrismServerAPI.instance.serverProperties.getAddress().host, PrismServerAPI.instance.serverProperties.getAddress().port)
+
+        extensionManager.gotoPostInit()
+    }
+
+    private fun setupProperties() {
+        if (PrismServerAPI.instance.serverProperties.isOnlineMode()) {
+            MojangAuth.init()
+        }
+
+        if (PrismServerAPI.instance.serverProperties.isBungeeCordSupport()) {
+            BungeeCordProxy.enable()
+        }
+
+        if (PrismServerAPI.instance.serverProperties.isBungeeCordSupport() && PrismServerAPI.instance.serverProperties.isVelocitySupport()) {
+            MinecraftServer.LOGGER.error("Bungeecord-Support and Velocity-Support conflict with each-other. Disabling both.")
+            this.properties.setProperty("velocity-support", false)
+            this.properties.setProperty("buneecord-support", false)
+            this.properties.updateDocument()
+            this.properties.reload()
+        }
+
+        if (PrismServerAPI.instance.serverProperties.isVelocitySupport()) {
+            VelocityProxy.enable(PrismServerAPI.instance.serverProperties.getVelocitySecretKey())
+        }
     }
 }
